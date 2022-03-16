328c8b579dea7a0d40976b400b5ebbcbc82629ec
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-03-16 13:43:27 +0100

    Construir el cliente

---
+++ src/main/java/MyBot.java
@@ -0,0 +1,13 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
+
+public class MyBot {
+    public static void main(String[] args) {
+        DiscordClient client = DiscordClient.create("OTUzNjMzMDMyMDg5MjcyMzQw.YjHZ-A.EAiDkn7RQvQcX6WNjk3dSiKrWzM");
+
+
+    }
+}
