b70259a4166ee82deebd466a362914268003f7b7
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-03-16 13:53:52 +0100

    Comentarios

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -4,8 +4,12 @@ import reactor.core.publisher.Mono;
 
 public class MyBot {
     public static void main(String[] args) {
+        //Utilizamos el DiscordClient, que es la clase necesaria para interacutar con Discord
+        //creamos uno con el token de nuestro bot, el cual obtuve en el portal para desarrolladores de Discord, al crear el bot en mi aplicación
         DiscordClient client = DiscordClient.create("OTUzNjMzMDMyMDg5MjcyMzQw.YjHZ-A.EAiDkn7RQvQcX6WNjk3dSiKrWzM");
 
+        //Iniciamos sesión y le indicamos que hacer al bot con lo contenido en client.withGateway
+        //al poner Mono.empty estamos diciendo que no haga nada
         Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
 
         login.block();
