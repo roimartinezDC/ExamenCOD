44a26250a16ce9616eeec29d54a5fe5fd06a57b9
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-05-20 09:25:16 +0200

    (0)

--- src/main/java/MyBot.java
+++
@@ -1,9 +0,0 @@
-import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
-import reactor.core.publisher.Mono;
-
-public class MyBot {
-    public static void main(String[] args) {
-
-    }
-}
